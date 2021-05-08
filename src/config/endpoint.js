46ad62ab88c3ff65db561027927f87f4604ab812
mildiemilk <punthitra.dits@gmail.com>
2021-05-08 16:39:22 +0700

    Setup Axois

---
+++ src/config/endpoint.js
@@ -0,0 +1,5 @@
+const endpoint = {
+  UPLOAD_IMAGE: process.env.REACT_APP_CLOUD_URL,
+}
+
+export default endpoint
---
+++ src/services/index.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+const callService = async (url, options) => {
+  options.headers = {
+    Authorization: process.env.REACT_APP_TOKEN,
+  }
+  return axios({ url, ...options }).then((response) => response.data)
+}
+
+const apiService = {
+  get: (url, options = {}) =>
+    callService(url, {
+      ...options,
+      method: "GET",
+    }),
+  post: (url, options = {}) =>
+    callService(url, {
+      ...options,
+      data: { ...options },
+      method: "POST",
+    }),
+}
+
+export default apiService
