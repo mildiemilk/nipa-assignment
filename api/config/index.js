6346b9027f2f741c9a001219df7eb1ed4e2859a4
mildiemilk <punthitra.dits@gmail.com>
2021-05-11 21:45:22 +0700

    Create new api

--- api/config/index.js
+++ api/config/index.js
@@ -2,4 +2,10 @@ require("dotenv").config()
 
 module.exports = {
   PORT: process.env.PORT,
+  API_KEY: process.env.API_KEY,
+  AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+  PROJECT_ID: process.env.PROJECT_ID,
+  STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+  MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+  APP_ID: process.env.APP_ID,
 }
---
+++ api/models/info.js
@@ -0,0 +1,67 @@
+const uuid = require("uuid")
+const moment = require("moment")
+const R = require("ramda")
+const firestore = require("../helpers")
+const snapshotToArray = require("../utilities/snapShotToArray")
+
+const status = {
+  PENDING: "pending",
+}
+class User {
+  async get() {
+    const snapshot = await firestore.db.collection("info").get()
+    const result = snapshotToArray(snapshot)
+    return result
+  }
+  async create({ name, contact, description }) {
+    const id = uuid.v4()
+    const docRef = firestore.db.collection("info").doc(id)
+    await docRef.set({
+      name,
+      contact,
+      description,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      status: status.PENDING,
+      id,
+    })
+    return id
+  }
+  async update({ accountId, status }) {
+    const docRef = firestore.db.collection("info").doc(accountId)
+    await docRef.update({
+      status,
+      updatedAt: firestore.FieldValue.serverTimestamp(),
+    })
+  }
+  async sortBy({ sortKey, orderKey }) {
+    const snapshot = await firestore.db
+      .collection("info")
+      .orderBy(sortKey, orderKey)
+      .get()
+    const result = snapshotToArray(snapshot)
+    return result
+  }
+  async filter({ status, startTime, endTime, listBy = "createdAt" }) {
+    const collection = firestore.db.collection("info")
+    let timeResult
+    let statusResult
+    const start = moment(startTime).startOf("day").toString()
+    const end = moment(endTime).endOf("day").toString()
+    const startWithOffset = moment(start).utcOffset("+0700").toDate()
+    const endWithOffset = moment(end).utcOffset("+0700").toDate()
+    if (status) {
+      statusResult = await collection.where("status", "in", status).get()
+    } else {
+      statusResult = await collection.get()
+    }
+    timeResult = await collection
+      .where(listBy, ">=", startWithOffset)
+      .where(listBy, "<=", endWithOffset)
+      .get()
+    const statusArr = snapshotToArray(statusResult)
+    const timeArr = snapshotToArray(timeResult)
+    return R.intersection(statusArr, timeArr)
+  }
+}
+
+module.exports = User
